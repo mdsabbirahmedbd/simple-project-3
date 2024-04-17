cedfee6668093238c9270afe59678808fbcec689
sabbir ahmed <sabbirahmed.info33@gmail.com>
2024-04-17 12:01:51 +0600

    my four commit

---
+++ src/firbase/firebase_config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.Vite_apiKey,
+  authDomain: import.meta.env.Vite_authDomain,
+  projectId: import.meta.env.Vite_projectId,
+  storageBucket: import.meta.env.Vite_storageBucket,
+  messagingSenderId: import.meta.env.Vite_messagingSenderId,
+  appId:  import.meta.env.Vite_appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
